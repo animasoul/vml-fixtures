73abe630b4c10c0331b51fa5f39e1d6e964ed43e
abe <abel.rogers@gmail.com>
2023-09-22 13:04:05 +0100

    code updates for endCap

--- build/render.php
+++ build/render.php
@@ -15,9 +15,25 @@ $baseImageUrl= "https://api-test.graphicsystems.com/gsimages/".$data['customer']
 
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Vml Fixtures – hello from a dynamic block!', 'vml-fixtures' ); ?>
     
-        Hi, <?php echo esc_html( $data['customer'] ); ?>, <? echo esc_html( $data['customerName']); ?> ...
+        <div class="fix-header">
+			<div class="fix-header__left">
+				<div class="fix-header__customer">
+					Customer: <?php echo esc_html( $data['customer'] ); ?>, <? echo esc_html( $data['customerName']); ?>
+				</div>
+				<div class="fix-header__store">
+					Store: <?php echo esc_html( $data['store'] ); ?>, <? echo esc_html( $data['storeName']); ?>
+				</div>
+			</div>
+			<div class="fix-header__right">
+				<div class="fix-header__status">
+					Fixture: <?php echo esc_html( $data['fixture'] ); ?>
+				</div>
+				<div class="fix-header__fixture">
+					<div class="<?php echo esc_html( $data['customer'] ); ?>-img"></div>
+				</div>
+			</div>
+		</div>
 
 		<?php include_once 'fixtures/'.$data['fixture'].'.php'; ?>
     </div>
--- src/fixtures/endCap.php
+++ src/fixtures/endCap.php
@@ -6,22 +6,26 @@
                 alt="shelf one one for end cap"
             />
         </div>
-        <div class="shelf1-2"><img
+        <div class="shelf1-2">
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
                 alt="shelf one two for end cap"
             />
         </div>
-        <div class="shelf1-3"><img
+        <div class="shelf1-3">
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
                 alt="shelf one three for end cap"
             />
         </div>
-        <div class="shelf1-4"><img
+        <div class="shelf1-4">
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
                 alt="shelf one four for end cap"
             />
         </div>
-        <div class="shelf1-5"><img
+        <div class="shelf1-5">
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
                 alt="shelf one five for end cap"
             />
@@ -29,31 +33,31 @@
     </div>
     <div class="shelf2">
         <div class="shelf2-1">
-        <img
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two one for end cap"
             />
         </div>
         <div class="shelf2-2">
-        <img
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two two for end cap"
             />
         </div>
         <div class="shelf2-3">
-        <img
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two three for end cap"
             />
         </div>
         <div class="shelf2-4">
-        <img
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two four for end cap"
             />
         </div>
         <div class="shelf2-5">
-        <img
+            <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two five for end cap"
             />
