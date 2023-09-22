0e3e17c30eb207cb7efd88501fd8add10c391d17
abe <abel.rogers@gmail.com>
2023-09-22 17:49:23 +0100

    refactored php template to make it more generic

--- build/fixtures/endCap.php
+++ build/fixtures/endCap.php
@@ -1,165 +1,133 @@
-<div class="endCap">
-    <h3>Shelf 1</h3>
-    <hr />
-    <div class="shelf shelf1">
-        
-        <div class="shelfshelf1-1">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf one one for end cap"
-            />
-        </div>
-        <div class="shelf1-2">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf one two for end cap"
-            />
-        </div>
-        <div class="shelf1-3">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf one three for end cap"
-            />
-        </div>
-        <div class="shelf1-4">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf one four for end cap"
-            />
-        </div>
-        <div class="shelf1-5">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf one five for end cap"
-            />
-        </div>
-        <div class="shelf1-6">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf one six for end cap"
-            />
-        </div>
-    </div>
-    <h3>Shelf 2</h3>
-    <hr />
-    <div class="shelf shelf2">
-        <div class="shelf2-1">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf two one for end cap"
-            />
-        </div>
-        <div class="shelf2-2">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf two two for end cap"
-            />
-        </div>
-        <div class="shelf2-3">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf two three for end cap"
-            />
-        </div>
-        <div class="shelf2-4">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf two four for end cap"
-            />
-        </div>
-        <div class="shelf2-5">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
-                alt="shelf two five for end cap"
-            />
-        </div>
-    </div>
-    <h3>Shelf 3</h3>
-    <hr />
-    <div class="shelf shelf3">
-        <div class="shelf3-1">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf three one for end cap"
-            />
-        </div>
-        <div class="shelf3-2">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf three two for end cap"
-            />
-        </div>
-        <div class="shelf3-3">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf three three for end cap"
-            />
-        </div>
-        <div class="shelf3-4">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf three four for end cap"
-            />
-        </div>
-        <div class="shelf3-5">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf three five for end cap"
-            />
-        </div>
-    </div>
-    <h3>Shelf 4</h3>
-    <hr />
-    <div class="shelf shelf4">
-        <div class="shelf4-1">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-37_Thumb.jpg"; ?>
-                alt="shelf four one for end cap"
-            />
-        </div>
-        <div class="shelf4-2">
-            <div class="shelf4-2-1">
-                <img
-                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                    alt="shelf four two one for end cap"
-                />
-            </div>
-            <div class="shelf4-2-2">
-                <img
-                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                    alt="shelf four two two for end cap"
-                />
-            </div>
-            <div class="shelf4-2-3">
-                <img
-                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                    alt="shelf four two three for end cap"
-                />
-            </div>
-            <div class="shelf4-2-4">
-                <img
-                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                    alt="shelf four two four for end cap"
-                />
-            </div>
-        </div>
-        <div class="shelf4-3">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-89_Thumb.jpg"; ?>
-                alt="shelf four four for end cap"
-            />
-        </div>
-        <div class="shelf4-4">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
-                alt="shelf four five for end cap"
-            />
-        </div>
-        <div class="shelf4-5">
-            <img
-                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
-                alt="shelf four six for end cap"
-            />
-        </div>
-    </div>
-</div>
+<?php
+
+$jsonString = <<<JSON
+{
+    "shelves": [
+        {
+            "items": [
+                {"image": "SAI-Fall23-26.jpg"},
+                {"image": "SAI-Fall23-26.jpg"},
+                {"image": "SAI-Fall23-26.jpg"},
+                {"image": "SAI-Fall23-26.jpg"},
+                {"image": "SAI-Fall23-26.jpg"},
+                {"image": "SAI-Fall23-31.jpg"}
+            ]
+        },
+        {
+            "items": [
+                {"image": "SAI-Fall23-31.jpg"},
+                {"image": "SAI-Fall23-31.jpg"},
+                {"image": "SAI-Fall23-31.jpg"},
+                {"image": "SAI-Fall23-31.jpg"},
+                {"image": "SAI-Fall23-31.jpg"}
+            ]
+        },
+        {
+            "items": [
+                {"image": "SAI-Fall23-36.jpg"},
+                {"image": "SAI-Fall23-36.jpg"},
+                {"image": "SAI-Fall23-36.jpg"},
+                {"image": "SAI-Fall23-36.jpg"},
+                {"image": "SAI-Fall23-36.jpg"}
+            ]
+        },
+        {
+            "custom": true,
+            "items": [
+                [
+                    {"image": "SAI-Fall23-37_Thumb.jpg"}
+                ],
+                [
+                    {"image": "SAI-Fall23-36.jpg"},
+                    {"image": "SAI-Fall23-36.jpg"},
+                    {"image": "SAI-Fall23-36.jpg"},
+                    {"image": "SAI-Fall23-36.jpg"}
+                ],
+                [
+                    {"image": "SAI-Fall23-89_Thumb.jpg"}
+                ],
+                [
+                    {"image": "SAI-Fall23-26.jpg"}
+                ],
+                [
+                    {"image": "SAI-Fall23-36.jpg"}
+                ]
+            ]
+        }
+    ]
+}
+JSON;
+
+$config = json_decode($jsonString, true);
+
+// Error handling for JSON decoding
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die('Error decoding JSON: ' . json_last_error_msg());
+}
+
+function generateShelfHeader($shelfNumber) {
+    return "<h3>Shelf {$shelfNumber}</h3><hr />";
+}
+
+function generateItemImage($baseImageUrl, $shelfNumber, $sectionCount, $itemCount, $item) {
+    $altText = "shelf {$shelfNumber}-{$sectionCount}-{$itemCount} for end cap";
+    return "<img src='{$baseImageUrl}/Fall23/{$item['image']}' alt='{$altText}' />";
+}
+
+function generateItem($baseImageUrl, $shelfNumber, $sectionClass, $itemCount, $item) {
+    $class = "shelf{$shelfNumber}{$sectionClass}-{$itemCount}";
+    $altText = "shelf {$shelfNumber} section {$sectionClass} item {$itemCount} for end cap";
+    $imgSrc = "{$baseImageUrl}/Fall23/{$item['image']}";
+    
+    return "<div class='{$class}'><img src='{$imgSrc}' alt='{$altText}' /></div>";
+}
+
+function generateSection($baseImageUrl, $shelfNumber, $sectionCount, $sectionItems) {
+    $html = '';
+    foreach ($sectionItems as $itemCount => $item) {
+        // Using the array index as item count directly, starting from 1
+        $itemCount++;
+        $sectionClass = $sectionCount ? "-{$sectionCount}" : '';
+        $html .= generateItem($baseImageUrl, $shelfNumber, $sectionClass, $itemCount, $item);
+    }
+    return $html;
+}
+
+function generateShelf($baseImageUrl, $shelfNumber, $shelf) {
+    $html = generateShelfHeader($shelfNumber);
+    $html .= "<div class='shelf shelf{$shelfNumber}'>";
+    
+    if (!empty($shelf['custom'])) {
+        foreach ($shelf['items'] as $sectionCount => $sectionItems) {
+            // Using the array index as section count directly, starting from 1
+            $sectionCount++;
+            $html .= "<div class='shelf{$shelfNumber}-{$sectionCount}'>"
+                  . generateSection($baseImageUrl, $shelfNumber, $sectionCount, $sectionItems)
+                  . "</div>";
+        }
+    } else {
+        $html .= generateSection($baseImageUrl, $shelfNumber, null, $shelf['items']);
+    }
+    
+    $html .= "</div>";
+    return $html;
+}
+
+function generateShelves($config, $baseImageUrl) {
+    // Check if the required 'shelves' key exists in the config
+    if (!isset($config['shelves']) || !is_array($config['shelves'])) {
+        die('Invalid configuration: Missing or invalid "shelves" key');
+    }
+
+    $html = "<div class='endCap'>";
+    foreach ($config['shelves'] as $shelfIndex => $shelf) {
+        $shelfNumber = $shelfIndex + 1;
+        $html .= generateShelf($baseImageUrl, $shelfNumber, $shelf);
+    }
+    $html .= "</div>";
+    return $html;
+}
+
+// Generate and output shelves HTML
+echo generateShelves($config, $baseImageUrl);
+
