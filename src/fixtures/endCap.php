6b2b769e2ac43ab916d5ee6bf6cb0d681c671bde
abe <abel.rogers@gmail.com>
2023-09-22 14:34:11 +0100

    configured shelves

--- src/fixtures/endCap.php
+++ src/fixtures/endCap.php
@@ -1,6 +1,9 @@
 <div class="endCap">
-    <div class="shelf1">
-        <div class="shelf1-1">
+    <h3>Shelf 1</h3>
+    <hr />
+    <div class="shelf shelf1">
+        
+        <div class="shelfshelf1-1">
             <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
                 alt="shelf one one for end cap"
@@ -30,8 +33,16 @@
                 alt="shelf one five for end cap"
             />
         </div>
+        <div class="shelf1-6">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
+                alt="shelf one six for end cap"
+            />
+        </div>
     </div>
-    <div class="shelf2">
+    <h3>Shelf 2</h3>
+    <hr />
+    <div class="shelf shelf2">
         <div class="shelf2-1">
             <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
@@ -60,6 +71,94 @@
             <img
                 src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-31.jpg"; ?>
                 alt="shelf two five for end cap"
+            />
+        </div>
+    </div>
+    <h3>Shelf 3</h3>
+    <hr />
+    <div class="shelf shelf3">
+        <div class="shelf3-1">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf three one for end cap"
+            />
+        </div>
+        <div class="shelf3-2">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf three two for end cap"
+            />
+        </div>
+        <div class="shelf3-3">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf three three for end cap"
+            />
+        </div>
+        <div class="shelf3-4">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf three four for end cap"
+            />
+        </div>
+        <div class="shelf3-5">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf three five for end cap"
+            />
+        </div>
+    </div>
+    <h3>Shelf 4</h3>
+    <hr />
+    <div class="shelf shelf4">
+        <div class="shelf4-1">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-37_Thumb.jpg"; ?>
+                alt="shelf four one for end cap"
+            />
+        </div>
+        <div class="shelf4-2">
+            <div class="shelf4-2-1">
+                <img
+                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                    alt="shelf four two one for end cap"
+                />
+            </div>
+            <div class="shelf4-2-2">
+                <img
+                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                    alt="shelf four two two for end cap"
+                />
+            </div>
+            <div class="shelf4-2-3">
+                <img
+                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                    alt="shelf four two three for end cap"
+                />
+            </div>
+            <div class="shelf4-2-4">
+                <img
+                    src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                    alt="shelf four two four for end cap"
+                />
+            </div>
+        </div>
+        <div class="shelf4-3">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-89_Thumb.jpg"; ?>
+                alt="shelf four four for end cap"
+            />
+        </div>
+        <div class="shelf4-4">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-26.jpg"; ?>
+                alt="shelf four five for end cap"
+            />
+        </div>
+        <div class="shelf4-5">
+            <img
+                src=<?php echo $baseImageUrl."/Fall23/SAI-Fall23-36.jpg"; ?>
+                alt="shelf four six for end cap"
             />
         </div>
     </div>
