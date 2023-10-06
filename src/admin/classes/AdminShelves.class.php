b5a10876e9c1af6dc320f412d9b51268c19357b2
abe <abel.rogers@gmail.com>
2023-10-06 15:40:00 +0100

    refactored class

--- src/admin/classes/AdminShelves.class.php
+++ src/admin/classes/AdminShelves.class.php
@@ -1,76 +1,75 @@
 <?php
+
 class AdminShelves {
+    private const DESCRIPTION = 'Description';
+    private const WIDTH = 'Width';
+    private const HEIGHT = 'Height';
+    private const PRODUCT_ID = 'ProductID';
+    private const THARSTERN_CODE = 'TharsternCode';
+    private const HORIZONTAL = 'Horizontal';
+    private const VERTICAL = 'Vertical';
 
     public static function renderFaceData(array $faceData): string {
-        $output = '';
-
-        // Loop through each shelf group
-        foreach ($faceData as $shelfGroups) {
-            $output .= self::renderShelfGroups($shelfGroups);
-        }
-
-        return $output;
+        return array_reduce($faceData, function ($carry, $shelfGroup) {
+            return $carry . self::renderShelfGroups($shelfGroup);
+        }, '');
     }
 
     private static function renderShelfGroups(array $shelfGroups): string {
-        $output = '';
-
-        foreach ($shelfGroups as $horizontalGroupName => $horizontalGroupItems) {
-            $output .= self::renderHorizontalGroup($horizontalGroupName, $horizontalGroupItems);
-        }
-
-        return $output;
+        return array_reduce(array_keys($shelfGroups), function ($carry, $groupName) use ($shelfGroups) {
+            return $carry . self::renderHorizontalGroup($groupName, $shelfGroups[$groupName]);
+        }, '');
     }
 
     private static function renderHorizontalGroup(string $horizontalGroupName, array $horizontalGroupItems): string {
-        $shelfNumber = preg_replace("/\D/", "", $horizontalGroupName); // Extracting shelf number
-        $output = "<div class='shelf shelf-{$shelfNumber}'><h2>Shelf {$shelfNumber}</h2>";
+        $shelfNumber = self::extractShelfNumber($horizontalGroupName);
+        $items = array_reduce($horizontalGroupItems, function ($carry, $items) {
+            return $carry . self::renderItems($items);
+        }, '');
 
-        foreach ($horizontalGroupItems as $items) {
-            foreach ($items as $itemGroup) {
-                $output .= self::renderItemGroup($itemGroup);
-            }
-        }
+        return "<div class='shelf shelf-{$shelfNumber}'><h2>Shelf {$shelfNumber}</h2>{$items}</div>";
+    }
 
-        $output .= "</div>";
-
-        return $output;
+    private static function renderItems(array $items): string {
+        return array_reduce($items, function ($carry, $itemGroup) {
+            return $carry . self::renderItemGroup($itemGroup);
+        }, '');
     }
 
     private static function renderItemGroup($itemGroup): string {
-        if (!is_array($itemGroup)) {
-            $itemGroup = [$itemGroup];
-        }
-
-        $output = '';
-        $output .= "<div class='item-group'>";
-        foreach ($itemGroup as $item) {
-            $output .= self::renderItemDiv($item);
-        }
-        $output .= "</div>";
-        return $output;
+        $items = is_array($itemGroup) ? $itemGroup : [$itemGroup];
+        return "<div class='item-group'>" . array_reduce($items, function ($carry, $item) {
+            return $carry . self::renderItemDiv($item);
+        }, '') . "</div>";
     }
 
     private static function renderItemDiv(array $item): string {
-        $description = htmlspecialchars($item['Description'], ENT_QUOTES);
-        $width = htmlspecialchars($item['Width'], ENT_QUOTES);
-        $height = htmlspecialchars($item['Height'], ENT_QUOTES);
-        $productID = htmlspecialchars($item['ProductID'], ENT_QUOTES);
-        $tharsternCode = htmlspecialchars($item['TharsternCode'], ENT_QUOTES);
-        $horizontal = htmlspecialchars($item['Horizontal'], ENT_QUOTES);
-        $vertical = htmlspecialchars($item['Vertical'], ENT_QUOTES);
+        $details = self::formatDetails($item);
+        $productID = htmlspecialchars($item[self::PRODUCT_ID], ENT_QUOTES);
 
-        return "
-            <div class='item' data-product-id='{$productID}' style='width:{$width}em;height:{$height}em'>
-                <p class='smallp'>{$description}</p>
-                <div class='details'>
-                    <p><strong>Description:</strong> {$description}</p>
-                    <p><strong>Width:</strong> {$width}</p>
-                    <p><strong>Height:</strong> {$height}</p>
-                    <p><strong>TharsternCode:</strong> {$tharsternCode}</p>
-                    <p><strong>Horizontal:</strong> {$horizontal}</p>
-                    <p><strong>Vertical:</strong> {$vertical}</p>
-                </div>
-            </div>";
+        return "<div class='item' data-product-id='{$productID}' style='width:{$details[self::WIDTH]}em;height:{$details[self::HEIGHT]}em'>
+            <p class='smallp'>{$details[self::DESCRIPTION]}</p>
+            <div class='details'>{$details['formatted']}</div>
+        </div>";
+    }
+
+    private static function formatDetails(array $item): array {
+        $fields = [self::DESCRIPTION, self::WIDTH, self::HEIGHT, self::THARSTERN_CODE, self::HORIZONTAL, self::VERTICAL];
+        $formattedDetails = [];
+
+        foreach ($fields as $field) {
+            $formattedDetails[$field] = htmlspecialchars($item[$field], ENT_QUOTES);
+        }
+
+        $details = array_reduce($fields, function ($carry, $field) use ($formattedDetails) {
+            return $carry . "<p><strong>{$field}:</strong> {$formattedDetails[$field]}</p>";
+        }, '');
+
+        $formattedDetails['formatted'] = $details;
+        return $formattedDetails;
+    }
+
+    private static function extractShelfNumber(string $horizontalGroupName): string {
+        return preg_replace("/\D/", "", $horizontalGroupName);
     }
 }
