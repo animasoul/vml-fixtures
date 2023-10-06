df1e4b8af09e8d45d5e71d2183fb5b2c415442ba
abe <abel.rogers@gmail.com>
2023-10-06 15:43:16 +0100

    refactored

--- src/admin/admin.php
+++ src/admin/admin.php
@@ -1,18 +1,20 @@
 <?php
+
 include_once plugin_dir_path(__FILE__) . '../helpers/Api.class.php';
 include_once 'classes/AdminShelves.class.php';
 
-class AdminFixtureRenderer
-{
+class AdminFixtureRenderer {
+    private const PANEL_KEY = 'Panel';
+    private const SHELF_KEY = 'Shelf';
+    private const HORIZONTAL_KEY = 'Horizontal';
+    private const VERTICAL_KEY = 'Vertical';
     private Api $api;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->api = new Api();
     }
 
-    public function getDataParams(): array
-    {
+    public function getDataParams(): array {
         return [
             'storeRoot' => $_SESSION['Customer'],
             'storeCode' => $_SESSION['Store'],
@@ -21,61 +23,66 @@ class AdminFixtureRenderer
     }
 
     public function sortApiData(array $params): array {
-        // Fetch data from the API
         $data = $this->api->make_api_call('GetSephoraProducts', $params, false, 30);
-    
-        $panelData = [];
-        $faceDataGroups = [];
-    
-        // 1. Split the data into panelData and faceDataGroups based on the Panel attribute.
-        foreach ($data as $item) {
-            if ($item['Panel'] === true) {
-                $panelData[] = $item;
-            } else {
-                // Group faceData items by Shelf and then by Horizontal.
-                $faceDataGroups[$item['Shelf']][$item['Horizontal']][] = $item;
-            }
-        }
-    
-        // 2. Sort panelData by Shelf and then by Horizontal.
-        usort($panelData, function ($a, $b) {
-            // First compare by Shelf
-            if ($a['Shelf'] !== $b['Shelf']) {
-                return $a['Shelf'] <=> $b['Shelf'];
-            }
-            // Then compare by Horizontal if Shelf values are the same
-            return $a['Horizontal'] <=> $b['Horizontal'];
-        });
-    
-        // 3. Organize faceData by Shelf and, if applicable, by Horizontal.
-        $formattedFaceData = [];
-        ksort($faceDataGroups); // Sort shelves in ascending order
-        foreach ($faceDataGroups as $shelf => $horizontals) {
-            $shelfGroup = [];
-            ksort($horizontals); // Sort horizontal values in ascending order
-            foreach ($horizontals as $items) {
-                // Sort items by Vertical within the Horizontal group.
-                usort($items, function ($a, $b) {
-                    return $a['Vertical'] <=> $b['Vertical'];
-                });
-                
-                // If there's more than one item with the same Horizontal, group them.
-                // if (count($items) > 1) {
-                    $shelfGroup[] = ['Horizontal: ' . count($items) => $items];
-                // } else {
-                //     // Otherwise, just append the individual items.
-                //     $shelfGroup = array_merge($shelfGroup, $items);
-                // }
-            }
-            // Append the entire Shelf group.
-            $formattedFaceData[] = ['Shelf: ' . $shelf => $shelfGroup];
-        }
-    
-        // Return the organized data
+
+        // Split the data into panelData and faceDataGroups based on the Panel attribute.
+        [$panelData, $faceDataGroups] = $this->splitData($data);
+
+        // Sort panelData by Shelf and then by Horizontal.
+        $this->sortPanelData($panelData);
+
+        // Organize faceData by Shelf and, if applicable, by Horizontal.
+        $formattedFaceData = $this->organizeFaceData($faceDataGroups);
+
         return [
             'panelData' => $panelData,
             'faceData' => $formattedFaceData,
         ];
+    }
+
+    private function splitData(array $data): array {
+        $panelData = [];
+        $faceDataGroups = [];
+
+        foreach ($data as $item) {
+            if ($item[self::PANEL_KEY] === true) {
+                $panelData[] = $item;
+            } else {
+                $faceDataGroups[$item[self::SHELF_KEY]][$item[self::HORIZONTAL_KEY]][] = $item;
+            }
+        }
+
+        return [$panelData, $faceDataGroups];
+    }
+
+    private function sortPanelData(array &$panelData): void {
+        usort($panelData, function ($a, $b) {
+            if ($a[self::SHELF_KEY] !== $b[self::SHELF_KEY]) {
+                return $a[self::SHELF_KEY] <=> $b[self::SHELF_KEY];
+            }
+            return $a[self::HORIZONTAL_KEY] <=> $b[self::HORIZONTAL_KEY];
+        });
+    }
+
+    private function organizeFaceData(array $faceDataGroups): array {
+        $formattedFaceData = [];
+        ksort($faceDataGroups);
+
+        foreach ($faceDataGroups as $shelf => $horizontals) {
+            $shelfGroup = [];
+            ksort($horizontals);
+
+            foreach ($horizontals as $items) {
+                usort($items, function ($a, $b) {
+                    return $a[self::VERTICAL_KEY] <=> $b[self::VERTICAL_KEY];
+                });
+                $shelfGroup[] = ['Horizontal: ' . count($items) => $items];
+            }
+
+            $formattedFaceData[] = ['Shelf: ' . $shelf => $shelfGroup];
+        }
+
+        return $formattedFaceData;
     }
 }
 
@@ -83,9 +90,8 @@ $renderer = new AdminFixtureRenderer();
 $params = $renderer->getDataParams();
 $apiData = $renderer->sortApiData($params);
 $faceDataHtml = AdminShelves::renderFaceData($apiData['faceData']);
-// var_dump(json_encode($apiData));
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
 
-<?php echo $faceDataHtml; ?>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+    <?php echo $faceDataHtml; ?>
 </div>
