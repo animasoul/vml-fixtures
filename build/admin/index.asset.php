c72ba7464f8e229b98513117920818495cee551b
abe <abel.rogers@gmail.com>
2024-03-20 15:46:13 +0000

    many updates

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5cd49fee0b3a7e6492fe');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f7666c7a36778fe4f1c0');
--- build/stores/index.asset.php
+++ build/stores/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f5aa4e0fb37db563c13f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'eece2e009c1ae9418e66');
