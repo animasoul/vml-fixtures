9a930b18998f78f415474a81e0c14c0364f6481b
abe <abel.rogers@gmail.com>
2026-05-18 11:15:51 +0100

    instruction sheet changes

--- build/instruction/view.asset.php
+++ build/instruction/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'd83904f63a8dc0876388');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'eac72c1b606627eee177');
